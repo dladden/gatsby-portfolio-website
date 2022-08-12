b113d998f1f892e7d7d6bca0338b1bf34aa72700
dladden <dladden77@gmail.com>
2022-08-12 08:50:48 -0400

    wrap page components and navbar

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+const React = require("react")
+const Layout = require("./src/components/Layout").default
+
+//wrap function provided by gatsby with two elements to pass:
+//elements and props.
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react"
-import logo from "../assets/images/logo.svg"
+import logo from "../assets/images/portfolio-logo-01.svg"
 import { FaAlignRight } from "react-icons/fa"
 import pageLinks from "../constants/links"
 import { Link } from "gatsby"
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="DL logo" />
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
